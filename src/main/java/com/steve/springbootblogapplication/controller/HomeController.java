c8aa3812046b97df1cf140169c4a3462ecf9c7a9
stevematdavies <stevematdavies@gmail.com>
2022-11-30 12:16:51 +0200

    Adding a controller for home and a template view

---
+++ src/main/java/com/steve/springbootblogapplication/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.steve.springbootblogapplication.controller;
+
+import com.steve.springbootblogapplication.model.Post;
+import com.steve.springbootblogapplication.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    private PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public String home(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+
+}
