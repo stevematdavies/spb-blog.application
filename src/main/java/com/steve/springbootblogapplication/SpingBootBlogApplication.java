2b943b739c0c119e4b04e4e73ec958622bbce8ca
stevematdavies <stevematdavies@gmail.com>
2022-11-30 11:11:07 +0200

    First Commit

---
+++ src/main/java/com/steve/springbootblogapplication/SpingBootBlogApplication.java
@@ -0,0 +1,13 @@
+package com.steve.springbootblogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steve/springbootblogapplication/SpingBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.springbootblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
