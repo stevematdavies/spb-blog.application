0fdb89c8cac8edf21ac437c1a4725d9f371fcc94
stevematdavies <stevematdavies@gmail.com>
2022-11-30 14:30:31 +0200

    Adding login and registration handling

--- src/main/java/com/steve/springbootblogapplication/controller/LoginController.java
+++ src/main/java/com/steve/springbootblogapplication/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.steve.springbootblogapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,13 @@ public class LoginController {
     public String getLoginPage(){
         return "login";
     }
+
+    @PostMapping
+    public String doLogin(){
+        return "login";
+    }
+
+
+
 
 }
--- src/main/java/com/steve/springbootblogapplication/controller/RegistrationController.java
+++ src/main/java/com/steve/springbootblogapplication/controller/RegistrationController.java
@@ -1,15 +1,36 @@
 package com.steve.springbootblogapplication.controller;
 
+import com.steve.springbootblogapplication.model.Account;
+import com.steve.springbootblogapplication.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
 
+    private final AccountService accountService;
+
+    @Autowired
+    public RegistrationController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @GetMapping
-    public String getRegistrationPage(){
+    public String getRegistrationPage(Model model){
+        model.addAttribute("account", new Account());
         return "register";
     }
+
+    @PostMapping
+    public String doRegister(@ModelAttribute Account account){
+        accountService.save(account);
+        return "redirect:/";
+    }
+
 }
