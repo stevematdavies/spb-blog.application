a9909d69708a6ac28ecad131933b32312faa2589
stevematdavies <stevematdavies@gmail.com>
2022-11-30 12:43:13 +0200

    Adding post route and 404 route

--- src/main/java/com/steve/springbootblogapplication/controller/PostController.java
+++ src/main/java/com/steve/springbootblogapplication/controller/PostController.java
@@ -9,22 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
-    private PostService postService;
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
 
     @Autowired
-    public HomeController(PostService postService) {
+    public PostController(PostService postService) {
         this.postService = postService;
     }
 
-    @GetMapping
-    public String home(Model model){
-        List<Post> posts = postService.getAll();
-        model.addAttribute("posts", posts);
-        return "home";
+    @GetMapping("/{id}")
+    public String getPost(@PathVariable Long id, Model model){
+        Optional<Post> maybePost = postService.getById(id);
+        if (maybePost.isPresent()){
+            model.addAttribute("post", maybePost.get());
+            return "post";
+        } else {
+            return "404";
+        }
     }
 }
